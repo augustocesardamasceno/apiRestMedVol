edf63e2ab7fbbf4a04850b8b74fba2064466cc8f
augustocesardamasceno <augustoccmd@hotmail.com>
2023-07-17 11:49:37 -0300

    First commit

---
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,46 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.paciente.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+    //PADRÃO DTO
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
+        pacienteRepository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listarPacientes(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacaoPaciente){
+        return pacienteRepository.findAllByAtivoTrue(paginacaoPaciente).map(DadosListagemPaciente::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar (@RequestBody @Valid DadosAtualizacaoPaciente dadosAtualizacaoPaciente){
+        var paciente = pacienteRepository.getReferenceById(dadosAtualizacaoPaciente.id());
+        paciente.atualizarInformacoes(dadosAtualizacaoPaciente);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        var paciente = pacienteRepository.getReferenceById(id);
+        paciente.excluir();
+    }
+
+}
---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,50 @@
+package med.voll.api.endereco;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String complemento;
+    private String numero;
+
+
+
+    public Endereco(DadosEndereco dadosEndereco) {
+        this.logradouro = dadosEndereco.logradouro();
+        this.bairro = dadosEndereco.bairro();
+        this.cep = dadosEndereco.cep();
+        this.uf = dadosEndereco.uf();
+        this.cidade = dadosEndereco.cidade();
+        this.numero = dadosEndereco.numero();
+        this.complemento = dadosEndereco.complemento();
+    }
+
+    public void atualizarInformacoes(DadosEndereco dados) {
+        if (dados.logradouro() != null){
+            this.logradouro = dados.logradouro();
+        }
+        if (dados.bairro() != null){
+            this.bairro = dados.bairro();
+        }
+        if (dados.cep() != null){
+            this.cep = dados.cep();
+        }
+        if (dados.uf() != null){
+            this.uf = dados.uf();
+        }
+        if (dados.cidade() != null){
+            this.cidade = dados.cidade();
+        }
+        if (dados.numero() != null){
+            this.numero = dados.numero();
+        }
+        if (dados.complemento() != null){
+            this.complemento = dados.complemento();
+        }
+    }
+}
